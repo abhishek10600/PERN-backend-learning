efb27edf4b2bf374361368aa335f6260231dec3e
abhishek10600 <abhi10sheksharma@gmail.com>
2026-01-22 18:08:00 +0530

    feat: add zod validation middleware and schemas for product creation and update

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -6,12 +6,40 @@ import {
   deleteProduct,
   updateProduct,
 } from "../controllers/products";
+import { validateData } from "../middlewares/validationMiddleware";
+
+// *******This is the best approach*******
+// we can create validation using zod.
+import { z } from "zod";
+
+const createProductSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  price: z.number({ message: "price must be a number" }),
+});
+
+const updateProductSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  price: z.number({ message: "price must be a number" }).optional(),
+});
+
+// creating validation schema using the table schema using drizzle-zod (only for learning)
+// import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+// import { productsTable } from "../db/productsSchema";
+// import { z } from "zod";
+
+// const createProductSchema = createInsertSchema(productsTable).omit({
+//   id: true,
+// });
+
+// type ProductType = z.infer<typeof createProductSchema>;
 
 const router = Router();
 
 router.get("/", listProducts);
 router.get("/:id", getProductById);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
+router.post("/", validateData(createProductSchema), createProduct);
+router.put("/:id", validateData(updateProductSchema), updateProduct);
 router.delete("/:id", deleteProduct);
 export default router;
